8b33f450995e03186d0d3d59cc7d70e4dc7d806a
grape <leah.ren@hotmail.com>
2021-06-17 18:47:34 -0600

    index added

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const client = new commando.CommandoClient({
     commandPrefix: 'p?', // The prefix of your bot.
     unknownCommandResponse: false, // Set this to true if you want to send a message when a user uses the prefix not followed by a command
 })
+client.registry.registerDefaults()
+client.on('ready',()=>{
+    console.log(`Logged in and ready to be used.. use "${client.commandPrefix}help".`)
+})
+client.setProvider(
+    sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => new Commando.SQLiteProvider(db))
+).catch(console.error);
+client.login("ODU1MjEyNTk2NzQzMzcyODEx.YMvMzA.YgzDJ8FAXU3_N_TYnrsxHBkeobw")
