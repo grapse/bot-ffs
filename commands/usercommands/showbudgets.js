7ab44a72bed54c1918955e1eb482c8782428e910
grape <leah.ren@hotmail.com>
2021-06-18 01:48:31 -0600

    yeah

---
+++ commands/usercommands/showbudgets.js
@@ -0,0 +1,28 @@
+// Owner only. Returns dictionary of all user IDs and their fever points.
+
+const { Command } = require('discord.js-commando')
+
+class budgetCommand extends Command{
+
+    constructor(client){
+
+        super( client, {
+            name: 'showbudgets',
+            memberName: 'showbudgets',
+            group: 'test',
+            description: 'Shows all budgets.',
+            details: 'Shows all budgets.',
+			ownerOnly: true,
+			guarded: true,
+            examples: ['p?showbudgets']
+        })
+
+    }
+
+    run(msg){
+        msg.reply(feverpoints)
+    }
+
+}
+
+module.exports = budgetCommand
--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const client = new commando.CommandoClient({
 //Server specifics
 const submissionchannel = '855264608637681685'
 const modchannel = '855246091871584256'
+const botchannel = '855264608637681685'
+const storychannel = '855264608637681685'
 const pointemoji = '855263455681970205'
 //Point balances
 global.feverpoints = {
@@ -24,19 +26,40 @@ client.on('ready',()=>{
     console.log(`Logged in and ready to be used.. use "${client.commandPrefix}help".`)
 })
 
-//For reacts
+// For reacts
 client.on('message', message => {
 	if (message.channel.id === submissionchannel) {
 		message.react(pointemoji);
 	}
 });
 
+// Handle point budgets
 client.on("messageReactionAdd", function(messageReaction, user){
-    // only need if the reaction is not by bot & fever point emoji & in the right channel
+    // only need if the reaction is not by bot & fever point emoji & in the right channel 
     if(user.id != '855212596743372811' && messageReaction.emoji.id === pointemoji
         && messageReaction.message.channel.id === submissionchannel){
-        // if budget is 1 or more, remove from budget. otherwise, remove reaction and warn user
-        messageReaction.message.reply('test');
+        //if user does not have the role, remove it
+        let User = messageReaction.message.guild.member(client.users.cache.get(user.id));
+        console.log(User);
+        if(User.roles.cache.some(r=>r.name==="🎸Summer Event")){
+            // if budget is 1 or more, remove from budget. otherwise, remove reaction and warn user
+            if(user.id in feverpoints){  
+                if(feverpoints[user.id] > 0){
+                    feverpoints[user.id] -= 1;
+                }
+                else{
+                    messageReaction.message.reply('You are out of points!');
+                    messageReaction.message.reactions.resolve(messageReaction).users.remove(user.id);
+                }
+            }
+            else{  // Otherwise add to the dict
+                feverpoints[user.id] = 7;  // Starts with 7 because 1 has been used just now
+            }
+        }
+        else{  // remove the reaction
+            messageReaction.message.reply('You are not part of the event!');
+            messageReaction.message.reactions.resolve(messageReaction).users.remove(user.id);
+        }
     }
     
 });
